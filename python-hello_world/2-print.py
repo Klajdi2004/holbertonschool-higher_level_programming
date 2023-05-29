37c9d4e33f923deab5cf8934798d547ff6d7aa2d
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-05-29 13:22:16 +0200

    last commit for this task

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-a = '"'
-print(a + "Programming is like building a multilingual puzzle", end='\n')
+print('"', "Programming is like building a multilingual puzzle", end='\n', sep="")
