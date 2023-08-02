d7e5af0c9412277a2c85700109e1652c2a1143c7
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-08-02 15:16:19 -0700

    hi

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,42 +1,18 @@
 #!/usr/bin/python3
-
-import sys
+"""model state class"""
 from model_state import Base, State
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+import sys
 
-
-def listStateObj():
-    """
-    Class that lists all states in the given database using SQLALchemy
-    Its non-SQLAlchemy variant is provided for comparison
-    """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
-    # The commented out code works perfectly fine and does what we want.
-    # However, it does not utilize sqlalchemy so technically this is
-    # incorrect
-    """
-    q = engine.execute('SELECT * FROM states ORDER BY states.id')
-    available_tables = q.fetchall()
-
-    for x in available_tables:
-        print("{}: {}".format(x.id, x.name))
-    """
-    # This does use sqlalchemy, and in a way is much easier
-    # Using query is very useful in future tasks where trying
-    # the SQL variant... would be rather difficult
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(u, p, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    my_session = Session()
-
-    for state in my_session.query(State).order_by(State.id).all():
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
-
-    my_session.close()
-
-if __name__ == '__main__':
-    listStateObj()
+    session.close()
--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
-""" Importing """
-import sys
+"""model state class"""
 from model_state import Base, State
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-""" 8-model_state_fetch_first.py """
+from sqlalchemy.orm import Session
+import sys
 
-
-def listStateObj():
-    """
-    Class that lists only the first states in the given database
-    using SQLALchemy. If the database is empty, print 'Nothing'
-    """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(u, p, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    my_session = Session()
-
-    first_state = my_session.query(State).first()
-    if first_state is None:
-        print("Nothing")
+    session = Session(engine)
+    state = session.query(State).order_by(State.id)
+    if state.count() == 0:
+        print('Nothing')
     else:
-        print("{}: {}".format(first_state.id, first_state.name))
-
-    my_session.close()
-
-if __name__ == '__main__':
-    listStateObj()
+        state = state.first()
+        print("{}: {}".format(state.id, state.name))
+    session.close()
