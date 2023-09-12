e8969a96d99ea1047be07a7e47dc5910a5fb2185
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-09-12 11:43:28 +0200

    javascript script to turn the header red

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -3,4 +3,9 @@
 function TurnRed() {
     const header = document.querySelector('header')
     header.style.color = "#FF0000" 
-}
+};
+
+// Call the TurnRed function when the document is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    TurnRed();
+});
